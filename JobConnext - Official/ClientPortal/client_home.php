38cf130640c0667e714bf03849ff14211739ab70
Rayzen <johnraymondbarrogo08@gmail.com>
2025-03-29 16:49:48 +0800

    Barrogo_job-offered

--- JobConnext - Official/ClientPortal/client_home.php
+++ JobConnext - Official/ClientPortal/client_home.php
@@ -132,65 +132,7 @@
 
                                         </div>
 
-                                        <?php 
-                                            $job_offeredQRY = "SELECT * FROM tbl_client_jobpost";
-                                            $job_offeredEXE = mysqli_query($conn, $job_offeredQRY);
-                                                while ($row = mysqli_fetch_assoc($job_offeredEXE)){
-
-                                                $client_id = $row['client_id'];
-                                                $job_salary = $row['salary']; 
-                                                $num_applicants = $row['applicants'];
-                                                $yr_Exp = $row['year_exp'];
-                                                $job_loc = $row['location'];
-                                                $date_posted = $row['date_posted'];
-                                                $job_offer = $row['job_offer'];
-
-                                                
-                                                $job_offered_clientname = "SELECT * FROM tbl_client_information WHERE client_id = $client_id";
-                                                $job_ex_offered_name = mysqli_query($conn, $job_offered_clientname);
-                                                $getjobdata_offered = mysqli_fetch_assoc($job_ex_offered_name);
-
-                                                $clientoffered_Fname = $getjobdata_offered['firstname'];
-                                                $clientoffered_Mname = $getjobdata_offered['middlename'];
-                                                $clientoffered_Sname = $getjobdata_offered['lastname'];     
-                                                $company_name = $getjobdata_offered['company_name'];
-
-                                                $job_offered_companyname_QRY = "SELECT * FROM tbl_client_information";
-                                                $job_offered_companyEXE = mysqli_query($conn, $job_offered_companyname_QRY);
-                                                $job_offered_companyGET = mysqli_fetch_assoc($job_offered_companyEXE);
-
-                                                $company_name = $job_offered_companyGET['company_name'];  
-                                        ?>                                                                                  
-                                               
-                                        <?php 
-                                            
-                                        ?>
-                                        <div class="details">
-                                            <h3><?php echo $company_name?></h3>
-                                            <p>Php7,000₱-8,000₱• 5 Applicants • Active</p>
-                                        </div>
-                                    </div>
-                                    <div class="job-dates">
-                                        <div class="menu">•••</div>
-                                        <p>11/8/2024 to 11/13/2024</p>
-                                    </div>
-                                </div>
-                                <div class="job-body">
-                                    <div class="info">
-                                        <p>
-                                            <strong>Location:</strong>
-                                            Taguig
-                                        </p>
-                                        <p>
-                                            <strong>Years of experience:</strong> 2
-                                        </p>
-                                    </div>
-                                    <div class="skills">
-                                        <p><strong>Skills needed:</strong></p>
-                                        <span class="skill-tag yellow">Truck Driver</span>
-                                    </div>
-                                </div><br>
-                                <?php } ?>
+                            <?php echo include 'job_offered.php' ?>
                         </a>
                         <div class="job-footer">
                             <button onclick="showAlert()" style="border: none;">
---
+++ JobConnext - Official/ClientPortal/job_offered.php
@@ -0,0 +1,69 @@
+<?php 
+                $job_offeredQRY = "SELECT * FROM tbl_client_jobpost";
+                $job_offeredEXE = mysqli_query($conn, $job_offeredQRY);
+                    while ($row = mysqli_fetch_assoc($job_offeredEXE)){
+
+                    $client_id = $row['client_id'];
+                    $job_salary = $row['salary']; 
+                    $num_applicants = $row['applicants'];
+                    $yr_Exp = $row['year_exp'];
+                    $job_loc = $row['location'];
+                    $date_posted = $row['date_posted'];
+                    $job_offer = $row['job_offer'];
+                    $job_status = $row['job_status'];
+
+                    
+                    $job_offered_clientname = "SELECT * FROM tbl_client_information WHERE client_id = $client_id";
+                    $job_ex_offered_name = mysqli_query($conn, $job_offered_clientname);
+                    $getjobdata_offered = mysqli_fetch_assoc($job_ex_offered_name);
+
+                    $clientoffered_Fname = $getjobdata_offered['firstname'];
+                    $clientoffered_Mname = $getjobdata_offered['middlename'];
+                    $clientoffered_Sname = $getjobdata_offered['lastname'];     
+                    $company_name = $getjobdata_offered['company_name'];
+
+                    $job_offered_companyname_QRY = "SELECT * FROM tbl_client_information";
+                    $job_offered_companyEXE = mysqli_query($conn, $job_offered_companyname_QRY);
+                    $job_offered_companyGET = mysqli_fetch_assoc($job_offered_companyEXE);
+
+                    $company_name = $job_offered_companyGET['company_name'];  
+
+                        function jobstatus($job_status){
+                            if ($job_status == null){
+                                echo "Inactive";
+                            }else{
+                                echo "Active";
+                            }
+                        }
+
+            ?>                                                                                  
+                    
+            <?php 
+                
+            ?>
+            <div class="details">
+                <h3><?php echo $company_name?></h3>
+                <p><?php echo " "."•"." "."Php". $job_salary. " "."•"." ". $num_applicants." "."Applicants". jobstatus($job_status) ?></p>
+            </div>
+        </div>
+        <div class="job-dates">
+            <div class="menu">•••</div>
+            <p>11/8/2024 to 11/13/2024</p>
+        </div>
+    </div>
+    <div class="job-body">
+        <div class="info">
+            <p>
+                <strong>Location:</strong>
+                Taguig
+            </p>
+            <p>
+                <strong>Years of experience:</strong> 2
+            </p>
+        </div>
+        <div class="skills">
+            <p><strong>Skills needed:</strong></p>
+            <span class="skill-tag yellow">Truck Driver</span>
+        </div>
+    </div><br>
+<?php } ?>
